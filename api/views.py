3581e61f7475300824fe7a41b16b5f62a5dd8eb5
mreza <you@example.com>
2025-11-23 02:07:54 +0330

    feat: Implement Category CRUD with Generic APIViews

--- api/views.py
+++ api/views.py
@@ -1,3 +1,57 @@
+from django.core.serializers import serialize
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView, status
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.exceptions import ValidationError
+from django.db import IntegrityError
 
+from .serializers import CategorySerializer
+from .models import *
 # Create your views here.
+
+'''class CategoryListCreateAPIView(APIView):
+    def get(self, request):
+        categories = Category.objects.filter(user=request.user)
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.Http_201_CREATED)'''
+
+class CategoryListCreateAPIView(generics.ListAPIView):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+
+    def preform_create(self, serializer):
+        try:
+            serializer.save(user=self.request.user)
+        except IntegrityError:
+            raise ValidationError({'detail': 'خطای دیتابیس هنگام اپدیت دسته'})
+
+class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+
+    def preform_update(self, serializer):
+        try:
+            serializer.save(user=self.request.user)
+        except IntegrityError:
+            raise ValidationError({'detail': 'خطای دیتابیس هنگام اپدیت دسته'})
+
+    def preform_destroy(self, instance):
+        if instance.transactions.exists():
+            raise ValidationError({'detail':'عدم امکان حذف زیرا تراکنش هایی به آن مرنبط است'})
+        instance.delete()
+
+
